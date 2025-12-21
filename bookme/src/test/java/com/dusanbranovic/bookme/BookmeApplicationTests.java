b769ac5de2513078fd8d64913ed926c5d7dfa285
Braxon2 <dusanb80@gmail.com>
2025-12-21 18:35:11 +0100

    Initial commit

---
+++ bookme/src/test/java/com/dusanbranovic/bookme/BookmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dusanbranovic.bookme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
